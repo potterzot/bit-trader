49879820bf02afeee204d16b146dc8fe5e6e5036
Nicholas Potter <potter.nicholas@gmail.com>
2013-04-22 08:29:16 -0700

    clean up unnecessary files

--- build/lib/bittrader/lib/__init__.py
+++
@@ -1,9 +0,0 @@
-import bittrader.lib
-from bittrader.lib.bitfloor import BitFloor
-from bittrader.lib.mtgox import MtGox
-from bittrader.lib.campbx import CampBX
-from bittrader.lib.bitce import BitCE
-#from . import campbx
-#from . import mtgox
-
-__all__ = ['BitFloor', 'MtGox', 'CampBX', 'BitCE']
